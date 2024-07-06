70e13b3835349914ac245c1c280d80bbc880e0fc
yuma0306 <yuma0306895@gmail.com>
2024-07-06 19:46:35 +0900

    fix header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,14 +32,15 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex gap-5 items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="/register">新規登録</a>
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ml-3">
+            <x-primary-button>
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
